7f474787867a9b46d44535f844d705d34f5e2eac
bmbaron <benjamin.m.baron@gmail.com>
2022-06-04 09:41:38 +0700

    Updates

---
+++ precache-manifest.a8dbb2b36618bf667a8cb5c53f13e74b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5f4164cbb3e6f361cee",
+    "url": "/react_practice_form/static/css/main.26c83b7c.chunk.css"
+  },
+  {
+    "revision": "b5f4164cbb3e6f361cee",
+    "url": "/react_practice_form/static/js/main.b5f4164c.chunk.js"
+  },
+  {
+    "revision": "072dbcb9417caa900b3a",
+    "url": "/react_practice_form/static/js/1.072dbcb9.chunk.js"
+  },
+  {
+    "revision": "369d1b54de9ae32d1154",
+    "url": "/react_practice_form/static/js/runtime~main.369d1b54.js"
+  },
+  {
+    "revision": "3500d043353bdf8b924321bb3d9d0a48",
+    "url": "/react_practice_form/index.html"
+  }
+];
